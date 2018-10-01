6d813f32255bd8b4549bdbe22f2d70e085c3f953
Deepika Patel <deepika.patel@justice.gov.uk>
2018-10-01 13:37:24 +0100

    Amended button on question1.html to use a conditional logic which has been added to routes.js.  question1.html was deleted as it was a duplicate of question0.html

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,22 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+// Branching
+// /q0form is a virutal page which in on page question0 on the form action
+
+router.post('/q0form', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+// case_name is the variable     case-start is the name in the govukRadios on page question0
+
+  let case_start = req.session.data['case-start']
+
+  if (case_start === 'before') {
+    res.redirect('/question1')
+  } else {
+    res.redirect('/results')
+  }
+})
